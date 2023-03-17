f4a4fc644ea5be90d1b51fb93f0700970cea51a3
VictorFolomeev <folomeevvictor@yandex.ru>
2023-03-17 13:31:03 +0300

    Условия if

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Привет')
+const age = 15;
+const gender = 'муж' // жен
+
+if (age > 18) {
+    console.log("Вам больше 18")
+}
+
+if (gender.toLocaleLowerCase() === 'муж') {
+    console.log('Вам налево');
+}
+
+if (gender.toLocaleLowerCase() === 'жен') {
+    console.log('Вам направо');
+}
