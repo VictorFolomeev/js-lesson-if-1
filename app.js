9c3f92233803fc74f5f08e83a7582cdda74f1fee
VictorFolomeev <folomeevvictor@yandex.ru>
2023-03-29 11:50:09 +0300

    Тернарный оператор 2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Проверка возраста //
+
 // let age = null;
 // const gender = 'муж' // жен
 
@@ -27,10 +29,34 @@
 // }
 // // Возраст
   
-const isAdmin = true;
+// Тернарный оператор
 
-console.log(`Добро пожаловать: ${
-    isAdmin
-    ? 'Администратор'
-    : 'Пользователь1'
-}`)
+// const isAdmin = true;
+
+// console.log(`Добро пожаловать: ${
+//     isAdmin
+//     ? 'Администратор'
+//     : 'Пользователь1'
+// }`)
+
+
+// const typeUser = "admin"; // user, guest, bot
+
+// switch (typeUser) {
+//     case 'admin':
+//         console.log('Вы администратор');
+//         break;
+//     case 'user':
+//         console.log('Вы пользователь');
+//         break;
+//     case 'quest':
+//         console.log('Вы гость');
+//         break;
+//     case 'bot':
+//         console.log('Вы бот');
+//         break;
+
+//     default:
+//         console.log('Неизвестный тип пользователь')
+//         break;    
+// }
