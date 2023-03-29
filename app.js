aee94f0260ee1c190be04486f4dc60727498901f
VictorFolomeev <folomeevvictor@yandex.ru>
2023-03-29 11:12:29 +0300

    Тернарный оператор

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-let age = null;
-const gender = 'муж' // жен
+// let age = null;
+// const gender = 'муж' // жен
 
-age = +age;
+// age = +age;
 
-if(!isNaN(age)) {
-    if (age > 18) {
-        console.log("Вам больше 18")
-    } else if (age < 18) {
-        console.log("Вам меньше 18 лет")
-    } else if (age === 18) {
-        console.log("Вам 18 лет")
-    } else {
-        console.log('Укажите Ваш возраст')
-    }    
-} else {
-    console.log('Введите корректные данные')
-}
+// if(!isNaN(age)) {
+//     if (age > 18) {
+//         console.log("Вам больше 18")
+//     } else if (age < 18) {
+//         console.log("Вам меньше 18 лет")
+//     } else if (age === 18) {
+//         console.log("Вам 18 лет")
+//     } else {
+//         console.log('Укажите Ваш возраст')
+//     }    
+// } else {
+//     console.log('Введите корректные данные')
+// }
 
 
-if (gender.toLocaleLowerCase() === 'муж') {
-    console.log('Вам налево');
-}
+// if (gender.toLocaleLowerCase() === 'муж') {
+//     console.log('Вам налево');
+// }
 
-if (gender.toLocaleLowerCase() === 'жен') {
-    console.log('Вам направо');
-}
-// Возраст
+// if (gender.toLocaleLowerCase() === 'жен') {
+//     console.log('Вам направо');
+// }
+// // Возраст
   
 const isAdmin = true;
 
